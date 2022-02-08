b91bf75d0e5a1414ec7d6c4f945a54f517313a04
Adnan Razzaq <adnan.razzaq@stud.uni-bamberg.de>
2022-02-08 03:06:39 +0100

    BST insert and search

--- BST/BST.js
+++ BST/BST.js
@@ -1,19 +1,93 @@
-function twoNumberSum(array, targetSum) {
-  // Write your code here.
-  const set = new Set();
-
-  for (const element of array) {
-    //if it exists in hash table
-    if (set.has(targetSum - element)) {
-      console.log(element);
-      console.log(set);
-      return;
-    } else {
-      set.add(element);
-    }
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 
-  return [];
+  /* O(n)/ O(1) */
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+
+    let currentNode = this;
+
+    while (true) {
+      if (value < currentNode.value) {
+        //we go on left
+        //check if left is null create a new node and break
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        //we go to right
+        //check if right node is null
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value === currentNode.value) return true;
+      if (value < currentNode.value) {
+        //go on left
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return false;
+  }
+
+  remove(value, parent = null) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    let currentNode = this;
+    //find the value to be removed
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parent = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parent = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        //value to delete is found
+        if (currentNode.left !== null && currentNode.right !== null) {
+          //find smallest value from right subtree
+          //in other words Replace with closest higher value
+          currentNode.value = currentNode.right.getMinimumValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+        }
+      }
+    }
+
+    return this;
+  }
 }
 
-twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+let bst = new BST(5);
+
+bst.insert(4);
+bst.insert(7);
+bst.insert(10);
+bst.insert(9);
+
+console.log(bst.contains(9));
