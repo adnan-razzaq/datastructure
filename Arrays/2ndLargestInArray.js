2a39c21309928a87248c5323b02096e32e113c84
Adnan Razzaq <adnan.razzaq@stud.uni-bamberg.de>
2022-08-20 19:50:36 +0200

    Second Largest in array with description

--- Arrays/2ndLargestInArray.js
+++ Arrays/2ndLargestInArray.js
@@ -1,5 +1,13 @@
 const { largest } = require("./LargestinArray.js");
-
+/* 
+!DESCRIPTION
+1 Find largest element in array.
+2 intialize secondLargestIndex as -1.
+3 loop through array and check all elements except the largest.
+4 Initially if 2ndLargest is -1 then update its value to currentIndex
+in first iteration.
+5 After that compare every current with secondlargest.
+*/
 function SecondLargest(array) {
     let largestIndex = largest(array);
     //Initialize result as -1
@@ -22,7 +30,17 @@ function SecondLargest(array) {
 
 SecondLargest([10, 10, 10]);
 
-///////////////////////////////////////////
+/* 
+!DESCRIPTION
+1 Consider data as stream of data.
+2 Keep track of both largest and secondLargest values.
+3 Loop through array and for each elemnt check
+3a if array[current]>array[largest]: largest=current,2ndLargest=largest
+3b if(array[current]===array[largest]) ignore
+3c if array[current]<array[largest]
+    if(secondlargst)===-1 secondlargest=current
+    if(array[current>array[secondLargest]]) secondLargest=current
+*/
 function EfficientSecondLargest(array) {
     let result = -1,
         largest = 0;
@@ -31,7 +49,7 @@ function EfficientSecondLargest(array) {
         if (array[index] > array[largest]) {
             result = largest;
             largest = index;
-        } else if (array[index] !== array[largest]) {
+        } else if (array[index] < array[largest]) {
             if (array[index] > array[result] || result === -1) {
                 result = index;
             }
